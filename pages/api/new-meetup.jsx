04f58a53e6dea728e941b3f2f61d3a4cf0a425c0
Joshua Holtsclaw <jildsmiles@yahoo.com>
2023-02-21 15:19:36 -0500

    adding api folder and connecting mongoDB

---
+++ pages/api/new-meetup.jsx
@@ -0,0 +1,25 @@
+import { MongoClient } from 'mongodb';
+
+// api never runs on client
+
+// /api/new-meetup  would be the api route
+
+async function handler(req, res) {
+	if (req.method === 'POST') {
+		const data = req.body;
+
+		const client = await MongoClient.connect(
+			'mongodb+srv://{process.env.MONGO_USERNAME}:{process.env.MONGO_PASS}@nextjs-practice.7mziiho.mongodb.net/?retryWrites=true&w=majority'
+		);
+		const db = client.db();
+		const collection = db.collection('meetups');
+
+		const result = await collection.insertOne(data);
+
+		console.log(result);
+
+		client.close();
+
+		res.status(201).json({ message: 'Meetup inserted!' });
+	}
+}
